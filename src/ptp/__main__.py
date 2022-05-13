a7b7f581d802850164948104f4ae3d508ada2efe
Willi Meierhof <Willi.Meierhof@tracetronic.de>
2022-05-13 15:40:21 +0200

    fix flake 8 stuff

--- src/ptp/__main__.py
+++ src/ptp/__main__.py
@@ -4,12 +4,11 @@ import click
 
 @click.command()
 @click.version_option()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
+@click.option("--count", default=1, help="Number of greetings.")
+@click.option("--name", prompt="Your name", help="The person to greet.")
 def main(count: int, name: str) -> str:
     """Python Test Project."""
-    for x in range(count):
+    for _ in range(count):
         click.echo(f"Hello {name}")
     return f"{count*name}"
 
