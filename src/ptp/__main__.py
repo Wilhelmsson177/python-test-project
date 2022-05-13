600bbf458f5985331445c345f52008cef617d102
Willi Meierhof <Willi.Meierhof@tracetronic.de>
2022-05-13 15:34:10 +0200

    Added new test and settings for vsw code

--- src/ptp/__main__.py
+++ src/ptp/__main__.py
@@ -4,8 +4,14 @@ import click
 
 @click.command()
 @click.version_option()
-def main() -> None:
+@click.option('--count', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Your name',
+              help='The person to greet.')
+def main(count: int, name: str) -> str:
     """Python Test Project."""
+    for x in range(count):
+        click.echo(f"Hello {name}")
+    return f"{count*name}"
 
 
 if __name__ == "__main__":
